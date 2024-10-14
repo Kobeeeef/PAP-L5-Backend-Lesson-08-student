bb6a75b83bf97e9d17c3b4b27015b44dc1d68b05
Kobe <83244503+Kobeeeef@users.noreply.github.com>
2024-10-13 19:49:45 -0700

    Initial commit

---
+++ users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id: 1,
+        name: "John Doe",
+        age: 43
+    },
+    {
+        id: 2,
+        name: "Jane Roe",
+        age: 41
+    },
+    {
+        id: 3,
+        name: "Sall McIntyre",
+        age: 21
+    },
+    {
+        id: 4,
+        name: "Bill McIntyre",
+        age: 25
+    }
+]
+
+// Practice Session
